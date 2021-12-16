7480b9b4d45420266cc17c08d31da8221806758d
Cristian Bello <cristian.bello6@gmail.com>
2021-12-15 19:05:08 -0500

    adjust character counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.count = this.count.bind(this);
+    this.state = {
+      counter: "",
+    };
+  }
+
+  count(event) {
+    this.setState({
+      counter: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.counter} onChange={this.count}></textarea>
+        <div className="counter">{this.state.counter.length}</div>
       </div>
     );
   }
